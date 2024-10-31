9cb183c0dfd653e7f854a6a28ecabff9ed5fa1c9
Nguyen Hoang Long <longnguyen5112000@gmail.com>
2024-10-31 10:03:07 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,53 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "webapistagev2.mytv.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7773.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7729.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7730.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7768.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7772.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7770.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7769.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7771.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "s7772.cdn.mytvnet.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "webappstage.mytv.vn",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
